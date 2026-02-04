7d949f392eb4773db495c8776eb6a5776f59ef21
kalkilovely <>
2026-02-04 00:29:45 +0000

    Variable calculations

---
+++ variables_and_types/variable_calculations/main.py
@@ -0,0 +1,10 @@
+# calculating the total cost of a grocery item
+item_name = "Soda"
+item_price = 6.99
+purchase_quantity = 5
+
+# total cost 
+total_cost = item_price * purchase_quantity
+
+# Testing
+print("Total cost for", purchase_quantity, "cases of", item_name, "is $", total_cost)
